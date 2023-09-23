677df7cf350404804e5490754d437589c6fc3188
Petre-Gaprindashvili <petre.gaprindashvili@bk.ru>
2023-09-23 23:52:18 +0400

    add html and css changes

--- main.js
+++ main.js
@@ -26,3 +26,43 @@ open.addEventListener("click", () => {
 close.addEventListener("click", () => {
   modal_container.classList.remove("show");
 });
+
+const openn = document.getElementById("openn");
+const modal_containerr = document.getElementById("modal_containerr");
+const closee = document.getElementById("closee");
+openn.addEventListener("click", () => {
+  modal_containerr.classList.add("show");
+});
+closee.addEventListener("click", () => {
+  modal_containerr.classList.remove("show");
+});
+
+const openit = document.getElementById("openit");
+const closeit = document.getElementById("closeit");
+const modal_containerit = document.getElementById("modal_containerit");
+openit.addEventListener("click", () => {
+  modal_containerit.classList.add("show");
+});
+closeit.addEventListener("click", () => {
+  modal_containerit.classList.remove("show");
+});
+
+const opento = document.getElementById("opento");
+const closeto = document.getElementById("closeto");
+const modal_containerto = document.getElementById("modal_containerto");
+opento.addEventListener("click", () => {
+  modal_containerto.classList.add("show");
+});
+closeto.addEventListener("click", () => {
+  modal_containerto.classList.remove("show");
+});
+
+const openon = document.getElementById("openon");
+const closeon = document.getElementById("closeon");
+const modal_containeron = document.getElementById("modal_containeron");
+openon.addEventListener("click", () => {
+  modal_containeron.classList.add("show");
+});
+closeon.addEventListener("click", () => {
+  modal_containeron.classList.remove("show");
+});
