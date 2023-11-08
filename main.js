33dc2b03d5a12090477439f42fdb18237a3d4610
Petre-Gaprindashvili <petre.gaprindashvili@bk.ru>
2023-11-09 02:11:21 +0400

    add some change

--- main.js
+++ main.js
@@ -3,8 +3,24 @@
 //   element.classList.toggle("dark-mode");
 // }
 
-const searching = document.querySelector(".searching");
-const inputsearching = document.querySelector(".inputsearching");
-searching.addEventListener("click", () => {
-  inputsearching.classList.toggle("show");
+// const searching = document.querySelector(".searching");
+// const inputsearching = document.querySelector(".inputsearching");
+// searching.addEventListener("click", () => {
+//   inputsearching.classList.toggle("show");
+// });
+
+const tabs = document.querySelectorAll("[data-tab-target]");
+const tabcontents = document.querySelectorAll("[data-tab-content]");
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabcontents.forEach((tabcontent) => {
+      tabcontent.classList.remove("active");
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
+    target.classList.add("active");
+  });
 });
