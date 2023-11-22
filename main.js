d515a4f71dcf6877a35bd461fd3c42ee1da4f037
Petre-Gaprindashvili <petre.gaprindashvili@bk.ru>
2023-11-23 00:06:31 +0400

    add some change

--- main.js
+++ main.js
@@ -1,41 +1,46 @@
-// function myfault() {
-//   const element = document.body;
-//   element.classList.toggle("dark-mode");
-// }
+const form = document.getElementById("form-in");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const login = document.getElementById("login");
+const validemail = document.getElementById("validemail");
+const validepass = document.getElementById("validepass");
 
-const searching = document.querySelector(".searching");
-const inputsearching = document.querySelector(".inputsearching");
-searching.addEventListener("click", () => {
-  inputsearching.classList.toggle("show");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 });
 
-function validate() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  const message = document.getElementById("message");
-  const message2 = document.getElementById("message2");
-  const login = document.getElementById("login");
-  const regex = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
+const emailvalidation = (email) => {
+  return email.match(
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+  );
+};
 
-  if (regex.test(email)) {
-    const email = document.getElementById("email");
-    email.style.border = "1px solid green";
+email.addEventListener("keyup", (e) => {
+  if (!emailvalidation(email.value)) {
+    validemail.innerText = "please fill the field";
+    return "";
   } else {
-    const email = document.getElementById("email");
+    validemail.style.color = "green";
+  }
+});
 
+password.addEventListener("keyup", (e) => {
+  if (!emailvalidation(password.value)) {
+    validepass.innerText = "please fill the field";
+    return "";
+  } else {
+    validepass.style.color = "green";
+  }
+});
+
+login.addEventListener("click", () => {
+  if (email.value.length === 0) {
     email.style.border = "1px solid red";
   }
-  if (password.trim().length >= 8) {
-    const password = document.getElementById("password");
-    password.style.border = "1px solid green";
-  } else {
-    const password = document.getElementById("password");
+});
+
+login.addEventListener("click", () => {
+  if (password.value.length === 0) {
     password.style.border = "1px solid red";
   }
-  if (email.length == 0 && password.length == 0) {
-    const malo = document.createElement("h5");
-    const node = document.createTextNode("ujgkugggkug");
-    malo.appendChild(node);
-    document.getElementById("mydiv").appendChild(malo);
-  }
-}
+});
